951b5573a2c063cd918bec8ea2194c97159eb8d3
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-03-04 10:30:20 +0700

    Initial Commit

---
+++ src/main/java/com/budimind/bashapp/BashAppApplication.java
@@ -0,0 +1,13 @@
+package com.budimind.bashapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BashAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budimind/bashapp/BashAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budimind.bashapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BashAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
